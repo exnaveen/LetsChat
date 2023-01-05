b95a570a1a1689aa6991273877b51a21bc2f7f67
exnaveen <exnaveeen@gmail.com>
2023-01-05 21:51:36 +0530

    This is the second iteration

---
+++ LetsChat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwsdybgAyqIregJZxi28acCv8JkOwH81o",
+    authDomain: "letschatapp-6267c.firebaseapp.com",
+    databaseURL: "https://letschatapp-6267c-default-rtdb.firebaseio.com",
+    projectId: "letschatapp-6267c",
+    storageBucket: "letschatapp-6267c.appspot.com",
+    messagingSenderId: "971622852260",
+    appId: "1:971622852260:web:a74a730635027e6f206831"
+  };
